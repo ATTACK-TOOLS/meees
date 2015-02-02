ecaa53214bfce24196560bfc14cfda1ffa035fd7
Matthew Canham <mdcanham@me.com>
2015-02-02 15:12:37 +1300

    Updated comments in the config file to make set up easier and updated instructions in the readme file.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# contact-me-contact-form
-A beautifully simple contact form made using PHP, HTML5, CSS &amp; Javascript
+# Contact Me Contact Form
+A beautiful, simple contact form made using PHP, HTML5, CSS &amp; Javascript
 
-Some documentation and a simplified integration process coming very soon!
+Adding a custom contact form to your website, without any 3rd party apps getting in the way, should be simple and easy. That's what this is - a contact form - simple.
+
+To get this up and running you'll need a web server running PHP - that's probably already installed in your server if you can get a static web page to load. After that copy this whole directory to your web server and load up the contact.php page, if you see some input boxes and a submit button then your web server is set up correctly. Not everything will be working at this point though, you'll need to follow the instructions below.
+
+To get this set up and working with your own email address you will need to go into the config.php file and change the variables to match your own settings. The config.php file has been commented to guide you through the set up process, of you have any questions just ask!
--- config.php
+++ config.php
@@ -1,13 +1,24 @@
 <?php
+/*	
+	Enter the name of your company or the name of your website here.
+	This will be used in the subject of the email that is sent to you and the body of the email that is sent to you.
+*/
 $companyName = "My Company";
 
-//SMTP Config
+/*
+	The next section is used to set up your SMTP settings, you can get these from your email hosting provider.
+	If you have the ability to set up multiple email accounts an idea could be to set up a contactform@yourdomain.com account and enter the details for this account in the section below.
+*/
 $smtpHost = "smtp.myCompany.com";
 $smtpPort = 465;
-$smtpUsername = "contactForm@myUsername.nz";
+$smtpUsername = "contactForm@myUsername.com";
 $smtpPassword = "securePasswordHere";
 
-$fromAddress = "contactForm@myUsername.nz";
-$toAddress = "info@myCompany.com";
-$toName = "Me, Myself and I";
+
+/*
+	Finally you will need to enter the details about the email that you will receive when someone submits an email on your contact form.
+*/
+$fromAddress = "contactForm@myUsername.com";
+$toAddress = "info@myCompany.com"; // This is the address (your address) that the email will be sent to when the contact form is submitted.
+$toName = "Me, Myself and I";// This is the name of the person (you) that will receive the email when the contact form is submitted.
 ?>
