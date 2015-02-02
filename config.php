68626aad2161da2c24b2856049d20a02551be6d0
Matthew Canham <mdcanham@me.com>
2015-02-02 15:21:54 +1300

    Slight update to config file

--- config.php
+++ config.php
@@ -9,16 +9,16 @@ $companyName = "My Company";
 	The next section is used to set up your SMTP settings, you can get these from your email hosting provider.
 	If you have the ability to set up multiple email accounts an idea could be to set up a contactform@yourdomain.com account and enter the details for this account in the section below.
 */
-$smtpHost = "smtp.myCompany.com";
+$smtpHost = "smtp.mycompany.com";
 $smtpPort = 465;
-$smtpUsername = "contactForm@myUsername.com";
+$smtpUsername = "contactForm@mycompany.com";
 $smtpPassword = "securePasswordHere";
 
 
 /*
 	Finally you will need to enter the details about the email that you will receive when someone submits an email on your contact form.
 */
-$fromAddress = "contactForm@myUsername.com";
-$toAddress = "info@myCompany.com"; // This is the address (your address) that the email will be sent to when the contact form is submitted.
+$fromAddress = "contactForm@mycompany.com";
+$toAddress = "info@mycompany.com"; // This is the address (your address) that the email will be sent to when the contact form is submitted.
 $toName = "Me, Myself and I";// This is the name of the person (you) that will receive the email when the contact form is submitted.
 ?>
